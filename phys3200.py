77e6c1a4758dd390690c10be17720811ec2d4c2b
Thomas Rimer <thorim3@gmail.com>
2025-04-24 23:11:16 -0400

    added graphing

--- phys3200.py
+++ phys3200.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 M_matrix = []
 K_columns = 6
@@ -28,5 +29,25 @@ f_k = np.array([[0.0, 1.0/2.0, 1.0, 1.0/2.0, 0, 0]])
 # Multiply f_k by the conjugate matrix
 f_k_conjugate = np.dot(f_k, M_conjugate_matrix)
 
+f_k_conjugate = 1.0/6.0 * f_k_conjugate
+
 for f_element in f_k_conjugate[0]:
     print(f_element.real, f_element.imag)
+
+# Plot the values
+plt.figure(figsize=(12, 6))
+
+plt.subplot(2, 1, 1)
+plt.plot(f_k_conjugate[0].real, marker='o', label='Real Part')  # Add color='blue' to the plot
+plt.axhline(y=0, color='black', linestyle='--')  # Add horizontal tick line at y=0
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(f_k_conjugate[0].imag, marker='o', label='Imaginary Part', color='orange')
+plt.axhline(y=0, color='black', linestyle='--')  # Add horizontal tick line at y=0
+
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.legend()
+plt.tight_layout()
+plt.show()
